00f5ac60ac0ef19ada9710b9518ca5f5afc8bd39
YashSharma80 <153749653+YashSharma80@users.noreply.github.com>
2026-03-30 09:49:38 +0530

    Update tts_local (1).py

--- tts_local (1).py
+++ tts_local (1).py
@@ -1,17 +1,18 @@
+//Source Code//
 from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
 from datasets import load_dataset
 import torch
 import soundfile as sf
-/////////
+
 print("Loading TTS model...")
-/////////
+
 processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
 model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
 vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
 
 embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
 speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
-////
+
 print("TTS Ready!")
 ////
 def text_to_speech(text):
@@ -27,3 +28,4 @@ def text_to_speech(text):
     sf.write(file_path, speech.numpy(), samplerate=16000)
 
     return file_path
+
